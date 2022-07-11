d4bdb37866cf7e1bc238bd243f9482f0d229ebac
Andreas Anckar <andreas.anckar@gmail.com>
2022-07-11 17:27:36 +0300

    Initial commit

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+export const schemaTypes = [
+  {
+    name: 'page',
+    title: 'Page',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+      },
+    ],
+  },
+]
