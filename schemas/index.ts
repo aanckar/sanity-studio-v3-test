d464463afbd17375123bef195880222ed41e130d
Andreas Anckar <andreas.anckar@gmail.com>
2022-08-08 14:53:35 +0300

    Add localized block

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,48 @@
 export const schemaTypes = [
   {
+    name: 'textBlock',
+    title: 'Text block',
+    type: 'object',
+    fields: [
+      {
+        name: 'text',
+        title: 'Text',
+        type: 'object',
+        fieldsets: [
+          {
+            title: 'Translations',
+            name: 'translations',
+            options: {collapsible: true},
+          },
+        ],
+        fields: [
+          {
+            name: 'en',
+            title: 'English',
+            type: 'string',
+            fieldset: null,
+          },
+          {
+            name: 'fi',
+            title: 'Finnish',
+            type: 'string',
+            fieldset: 'translations',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'blocks',
+    title: 'Content',
+    type: 'array',
+    of: [
+      {
+        type: 'textBlock',
+      },
+    ],
+  },
+  {
     name: 'page',
     title: 'Page',
     type: 'document',
@@ -13,6 +56,11 @@ export const schemaTypes = [
         name: 'image',
         title: 'Image',
         type: 'image',
+      },
+      {
+        name: 'blocks',
+        title: 'Blocks',
+        type: 'blocks',
       },
     ],
   },
